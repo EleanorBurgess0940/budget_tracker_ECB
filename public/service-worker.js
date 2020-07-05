9ec57900ea1ce8583285a436b3523b497cd4c1c0
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-07-05 17:31:40 -0400

    added comments and cleaned up code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,33 +1,44 @@
-console.log("hello!");
-
+//list of local resources we want to be cached
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
   "/styles.css",
   "/db.js",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png",
 ];
 
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
+//install
+//the install handler will take care of precaching resources
+//when the site is open this event will happen
 self.addEventListener("install", (event) => {
+  //will not install until the code is finished
   event.waitUntil(
-    console.log("test"),
     caches
+      //precache is created
       .open(PRECACHE)
+      //precache is created and all files are added
+      //can be seen on the application page of the console
       .then((cache) => cache.addAll(FILES_TO_CACHE))
-
+      //calls on the activate event
       .then(self.skipWaiting())
   );
 });
 
+//activate
+// The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", (event) => {
   const currentCaches = [PRECACHE, RUNTIME];
+  //will not activate until the code is finished
   event.waitUntil(
     caches
       .keys()
       .then((cacheNames) => {
+        //return cacheNames.filter
         return cacheNames.filter(
           (cacheName) => !currentCaches.includes(cacheName)
         );
@@ -43,7 +54,11 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+//fetch
+//This fetch handler will respond for same-origin resources from the cache
+//it will populate the run time cache with the response from the network
 self.addEventListener("fetch", (event) => {
+  // Skip cross-origin requests
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
@@ -55,6 +70,7 @@ self.addEventListener("fetch", (event) => {
           return fetch(event.request).then((response) => {
             console.log("response: ", response);
             console.log("event: ", event);
+            // Put a copy of the response in the runtime cache.
             return cache.put(event.request.url, response.clone()).then(() => {
               return response;
             });
